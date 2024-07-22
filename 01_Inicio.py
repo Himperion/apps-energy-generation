2b7536a225bf93944e8ed2ab1cf18a3e4db820bd
José Camilo <osiris931218@gmail.com>
2024-07-22 10:35:01 -0500

    Add files via upload

---
+++ 01_Inicio.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+text = {
+    "subheader_1" : "Página de pruebas para la implementación del proyecto **Diseño de un aplicativo para la estimación de la operación de sistemas de generación eléctrica a partir de balances de potencia y energía**"
+}
+
+
+st.markdown("# 🏠 Inicio")
+
+st.markdown(text["subheader_1"])
+
