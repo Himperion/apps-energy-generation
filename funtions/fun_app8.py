c6a6ded8c94d36b779b7008f6e5975a6e2c9cf7d
José Camilo <osiris931218@gmail.com>
2024-12-17 11:43:42 -0500

    ver_17.12.18

--- funtions/fun_app8.py
+++ funtions/fun_app8.py
@@ -1,22 +1,85 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
+import numpy as np
+import io
+from datetime import datetime
 
 from funtions import fun_app5
 
 #%%
 
+dict_phases = {
+    "Monofásico": {"Num": 1, "label": "1️⃣ Monofásico"},
+    "Trifásico": {"Num": 3, "label": "3️⃣ Trifásico"}
+}
 
-
+dict_months = {1: "enero",
+               2: "febrero",
+               3: "marzo",
+               4: "abril",
+               5: "mayo",
+               6: "junio",
+               7: "julio",
+               8: "agosto",
+               9: "septiembre",
+               10: "octubre",
+               11: "noviembre",
+               12: "diciembre"}
 
 #%% funtions general
+
+def name_file_head(name: str) -> str:
+    now = datetime.now()
+    return f"[{now.day}-{now.month}-{now.year}_{now.hour}-{now.minute}] {name}"
+
+def to_excel(df: pd.DataFrame):
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine="openpyxl") as writer:
+        df.to_excel(writer, index=False, sheet_name="Sheet1")
+
+    return output.getvalue()
 
 def get_label_params(dict_param: dict) -> str:
 
     return f"**{dict_param['label']}:** {dict_param['description']} {dict_param['unit']}"
 
-def solarGenerationOnGrid(df_data: pd.DataFrame, PV_data: dict, INV_data: dict, PVs: int, PVp: int, v_PCC: float, columnsOptionsData: list, params_PV: dict, rename_PV: dict, show_output: list):
+def getTimeData(df_data: pd.DataFrame) -> dict:
 
+    timeInfo = {}
+    numberRows = df_data.shape[0]
+
+    if "dates (Y-M-D hh:mm:ss)" in df_data.columns:
+        time_0 = df_data.loc[0, "dates (Y-M-D hh:mm:ss)"].to_pydatetime()
+        time_1 = df_data.loc[1, "dates (Y-M-D hh:mm:ss)"].to_pydatetime()
+
+        timeInfo["deltaMinutes"] = (time_1 - time_0).total_seconds()/60
+        timeInfo["dateIni"] = time_0
+        timeInfo["dateEnd"] = df_data.loc[df_data.index[-1], "dates (Y-M-D hh:mm:ss)"].to_pydatetime()
+        timeInfo["deltaDays"] = (numberRows*timeInfo["deltaMinutes"])/1440
+        timeInfo["years"] = df_data['dates (Y-M-D hh:mm:ss)'].dt.year.unique().tolist()
+
+        listAuxMonth, listAuxDayYear = [], []
+        for year in timeInfo["years"]:
+            df_year = df_data[df_data["dates (Y-M-D hh:mm:ss)"].dt.year == year]
+
+            list_month = df_year["dates (Y-M-D hh:mm:ss)"].dt.month.unique().tolist()
+            listAuxMonth.append(list_month)
+
+            listAuxDay = []
+            for i in range(0,len(list_month),1):
+                df_month = df_year[df_year["dates (Y-M-D hh:mm:ss)"].dt.month == list_month[i]]
+                listAuxDay.append(df_month["dates (Y-M-D hh:mm:ss)"].dt.day.unique().tolist())
+
+            listAuxDayYear.append(listAuxDay)
+
+        timeInfo["months"] = listAuxMonth
+        timeInfo["days"] = listAuxDayYear
+
+    return timeInfo
+
+def solarGenerationOnGrid(df_data: pd.DataFrame, PV_data: dict, INV_data: dict, PVs: int, PVp: int, V_PCC: float, columnsOptionsData: list, params_PV: dict, rename_PV: dict, show_output: list):
+    
     conditions = fun_app5.get_dataframe_conditions(df_data, columnsOptionsData)
     PV_params = fun_app5.get_PV_params(**PV_data)
 
@@ -29,11 +92,31 @@ def solarGenerationOnGrid(df_data: pd.DataFrame, PV_data: dict, INV_data: dict, 
                                          dict_conditions=columnsOptionsData,
                                          list_output=show_output)
     
-    st.dataframe(df_pv)
+    numberPhases = dict_phases[INV_data['phases']]['Num']
     
+    df_pv.rename(columns={'Impp(A)': 'I_PV(A)', 'Vmpp(V)': 'V_PV(V)', 'Pmpp(kW)': 'Pgen_PV(kW)'}, inplace=True)
+    df_pv['Pgen_AC(kW)'] = df_pv['Pgen_PV(kW)']*INV_data['efficiency_max']/100
+    df_pv['Pdem(kW)'] = df_pv['Load(kW)'] - df_pv['Pgen_AC(kW)']
+    df_pv['V_INV(V)'] = V_PCC
+    df_pv['I_INV(A)'] = (df_pv['Pgen_AC(kW)']*1000)/(np.sqrt(numberPhases)*V_PCC)
+    df_pv['V_LOAD(V)'] = V_PCC
+    df_pv['I_LOAD(A)'] = (df_pv['Load(kW)']*1000)/(np.sqrt(numberPhases)*V_PCC)
+    df_pv['V_DEM(V)'] = V_PCC
+    df_pv['I_DEM(A)'] = (df_pv['Pdem(kW)']*1000)/(np.sqrt(numberPhases)*V_PCC)
     
-    return
+    return df_pv
 
+def getFullReport(df_pv: pd.DataFrame, timeInfo: dict):
+
+    for i in range(0,len(timeInfo["years"]),1):
+        for j in range(0,len(timeInfo["months"][i]),1):
+
+            df_aux = df_pv[(df_pv["dates (Y-M-D hh:mm:ss)"].dt.year == timeInfo["years"][i]) & (df_pv["dates (Y-M-D hh:mm:ss)"].dt.month == timeInfo["months"][i][j])]
+            
+            print(timeInfo["years"][i], dict_months[timeInfo["months"][i][j]])
+            print(df_aux["Load(kW)"].sum()*(timeInfo["deltaMinutes"]/60))
+
+    return
 
 #%% funtions streamlit
 
--- pages/08_Generación solar On-Grid.py
+++ pages/08_Generación solar On-Grid.py
@@ -60,11 +60,14 @@ if 'check_PV' not in st.session_state:
 if 'check_INV' not in st.session_state:
     st.session_state['check_INV'] = False
 
+if 'dictDataOnGrid' not in st.session_state:
+    st.session_state['dictDataOnGrid'] = None
+
 #%% main
 
 st.markdown("# Generación Solar Off-Grid")
 
-tab1, tab2, tap3 = st.tabs(["📑 Marco teórico", "💾 Entrada de datos", "📝 Análisis"])
+tab1, tab2, tab3 = st.tabs(["📑 Marco teórico", "💾 Entrada de datos", "📝 Análisis"])
 
 to_yaml = {
     "PV": {"label": "🪟 Módulo fotovoltaico", "sheet_label": "PV", "name": "Módulo fotovoltaico", "emoji": "🪟"},
@@ -87,7 +90,7 @@ with tab2:
         with st.container(border=True):
             st.markdown("📋 **:blue[Datos de carga, temperatura de operación y potencial solar del sitio:]**")
 
-            uploadedYamlDATA = st.file_uploader(label="**Cargar archivo EXCEL**", type=["xlsx"], key='uploadedYamlDATA')
+            uploadedXlsxDATA = st.file_uploader(label="**Cargar archivo EXCEL**", type=["xlsx"], key='uploadedYamlDATA')
 
         with st.container(border=True):
             st.markdown(f"{dict_components['PV']['emoji']} **:blue[{dict_components['PV']['name']}:]**")
@@ -111,16 +114,15 @@ with tab2:
 
             st.markdown("⚡ **PCC (Punto de conexión común**")
 
-            v_PCC = st.number_input(label="Tensión del punto de conexión común", value=127.0, placeholder="Ingrese un valor",
+            V_PCC = st.number_input(label="Tensión del punto de conexión común", value=127.0, placeholder="Ingrese un valor",
                                     format="%0.1f", step=None, min_value=0.0, max_value=1000.0)
 
         submitted = st.form_submit_button("Aceptar")
 
         if submitted:
-
-            if uploadedYamlDATA is not None:
+            if uploadedXlsxDATA is not None:
                 try:
-                    df_data = pd.read_excel(uploadedYamlDATA)
+                    df_data = pd.read_excel(uploadedXlsxDATA)
                     df_data, validateEntries['check_DATA'], columnsOptionsData = fun_app8.check_dataframe_input(df_data, optionsColumnsUploadedDATA)
                 except:
                     st.error("Error al cargar archivo **EXCEL** (.xlsx)", icon="🚨")
@@ -145,25 +147,69 @@ with tab2:
                     st.error("Error al cargar archivo **YAML** (.yaml)", icon="🚨")
 
             else:
-                st.error("Cargar **Datos del módulo fotovoltaico**", icon="🚨")
+                st.error("Cargar **Datos del Inversor**", icon="🚨")
 
             if validateEntries['check_DATA'] and validateEntries['check_PV'] and validateEntries['check_INV']:
                 
-                dictDataOnGrid = {
+                st.session_state['dictDataOnGrid'] = {
                     'df_data': df_data,
                     'PV_data': PV_data,
                     'INV_data': INV_data,
                     'PVs': PVs,
                     'PVp': PVp,
-                    'v_PCC': v_PCC,
+                    'V_PCC': V_PCC,
                     'columnsOptionsData': columnsOptionsData,
                     'params_PV': params_PV,
                     'rename_PV': rename_PV,
                     'show_output': showOutput
                 }
 
-                fun_app8.solarGenerationOnGrid(**dictDataOnGrid)
+            else:
 
-                
+                st.text(validateEntries['check_DATA'])
+                st.text(validateEntries['check_PV'])
+                st.text(validateEntries['check_INV'])
+
+
+    if st.session_state['dictDataOnGrid'] is not None:
+        #
+        df_out = fun_app8.solarGenerationOnGrid(**st.session_state['dictDataOnGrid'])
+        bytesFile = fun_app8.to_excel(df_out)
+
+        df_download = st.download_button(
+            label="📄 Descargar **:blue[Dataset] XLSX**",
+            data=bytesFile,
+            file_name=fun_app8.name_file_head(name="dataset_OffGrid.xlsx"),
+            mime='xlsx')
+        
+with tab3:
+    
+    st.session_state['check_DATA'] = False
+    st.session_state['check_PV'] = False
+    st.session_state['check_INV'] = False
+    st.session_state['dictDataOnGrid'] = None
+
+
+    with st.form("analysisOnGrid"):
+        uploaderXlsx = st.file_uploader(label="**Cargar archivo EXCEL**", type=["xlsx"], key='uploaderXlsx')  
+
+        submitted = st.form_submit_button("Aceptar")
+
+        if submitted:
+            if uploaderXlsx is not None:
+                try:
+                    df_data = pd.read_excel(uploaderXlsx)
+                    timeInfo = fun_app8.getTimeData(df_data)
+
+                    for key, value in timeInfo.items():
+                        st.text(f"{key}: {value}")
+
+                    st.dataframe(df_data)
+                    
+                except:
+                    st.error("Error al cargar archivo **EXCEL** (.xlsx)", icon="🚨")
+            else:
+                st.error("Cargar **Dataset generación Off-Grid**", icon="🚨")
+    
 
         
