26d80f2b57a2f4cfe77bd35a5a5c3f558fb2177d
José Camilo <osiris931218@gmail.com>
2025-04-30 21:35:00 -0500

    ver_30.04.25-1

--- 01_Inicio.py
+++ 01_Inicio.py
@@ -3,7 +3,7 @@
 import streamlit as st
 
 text = {
-    "subheader_1" : "Página de pruebas para la implementación del proyecto **Diseño de un aplicativo para la estimación de la operación de sistemas de generación eléctrica a partir de balances de potencia y energía**"
+    "subheader_1" : "Implementación del proyecto **Diseño de un aplicativo para la estimación de la operación de sistemas de generación eléctrica a partir de balances de potencia y energía**"
 }
 
 st.sidebar.link_button(":violet-badge[**Ir a la app de herramientas**]", "https://app-nasa-power.streamlit.app/", icon="🔧")
